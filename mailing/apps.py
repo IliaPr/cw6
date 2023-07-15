6a0b36bae29039d1445825ad274452a7ea562858
IliaPr <ilya.prianichnikov@gmail.com>
2023-07-15 23:08:59 +0700

    mailing app and start of users app

---
+++ mailing/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class MailingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mailing'
+    verbose_name = 'сервис управления рассылками'
+
